c90489b63eb35cdf8e0eaf4ab17fe0ad837704ab
davit <davit.natenadze@gmail.com>
2023-06-05 13:21:16 +0200

    Initial Commit

---
+++ BetterSleep ML/BetterSleep_MLApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterSleep_MLApp.swift
+//  BetterSleep ML
+//
+//  Created by Davit Natenadze on 05.06.23.
+//
+
+import SwiftUI
+
+@main
+struct BetterSleep_MLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
